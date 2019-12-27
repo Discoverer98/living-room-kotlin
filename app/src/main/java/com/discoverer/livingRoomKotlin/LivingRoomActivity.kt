58779cec2b87b5a107e8ff05a362d11083527633
Nelson Leme <discovererxcviii@gmail.com>
2019-12-27 15:50:18 -0800

    Adding comments.

--- app/src/main/java/com/discoverer/livingRoomKotlin/LivingRoomActivity.kt
+++ app/src/main/java/com/discoverer/livingRoomKotlin/LivingRoomActivity.kt
@@ -7,7 +7,10 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.discoverer.livingRoomKotlin.databinding.ActivityLivingRoomBinding
 
-
+/**
+ * Sample Activity class just to show how you can make use of the data passed through the
+ * Activity Parameters idiom.
+ */
 class LivingRoomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +19,12 @@ class LivingRoomActivity : AppCompatActivity() {
         binding.arguments = Arguments.createFromIntent(intent)
     }
 
-
+    /**
+     * The actual Arguments class, that will encapsulate the mechanism for passing data to an Activity.
+     * Due to the modern features of Kotlin, we don't need to use an AutoValue.
+     * This class can be used for other purposes, like passing this data to a Fragment by generating a
+     * Bundle object with the data in it.
+     */
     class Arguments(val roomColor: String, val numOfRooms: Int) {
 
         companion object {
@@ -25,6 +33,9 @@ class LivingRoomActivity : AppCompatActivity() {
             private const val NUM_OF_WALLS_TAG = "numOfWallsTag"
             private const val DEFAULT_NUM_OF_WALLS = 4
 
+            /**
+             * Function to create an Arguments object from an Intent object.
+             */
             fun createFromIntent(intent: Intent): Arguments {
                 return Arguments(
                     roomColor = intent.getStringExtra(ROOM_COLOR_TAG),
@@ -33,6 +44,10 @@ class LivingRoomActivity : AppCompatActivity() {
 
         } // companion object
 
+        /**
+         * Function to start the Activity associated with this Arguments class, and that will put
+         * the required data in an Intent object.
+         */
         fun startActivity(context: Context) {
             val intent = Intent(context, LivingRoomActivity::class.java)
             intent.putExtra(ROOM_COLOR_TAG, roomColor)
--- app/src/main/java/com/discoverer/livingRoomKotlin/MainActivity.kt
+++ app/src/main/java/com/discoverer/livingRoomKotlin/MainActivity.kt
@@ -3,26 +3,34 @@ package com.discoverer.livingRoomKotlin
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.discoverer.livingRoomKotlin.databinding.ActivityMainBinding
 
-
+/**
+ * Sample class to show the usage of the Activity Parameters idiom.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Thanks to MVVM and Data Binding, we don't need to resort to ButterKnife to get the data from the UI.
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.mainViewModel = MainViewModel(this)
     }
 
 
+    /**
+     * A simple ViewModel class, which just responds to the "Enter Living Room" button click.
+     */
     class MainViewModel(private val context: Context) {
 
         fun enterLivingRoom(roomColor: String?, numOfRoomsStr: String?) {
+            // Even though there are only 2 Activity arguments being passed here, I'm using
+            // named parameters to show how it can replace the Builder pattern that is used
+            // with AutoValue when programming in Java.
             val arguments = LivingRoomActivity.Arguments(
-                roomColor = roomColor ?: "", 
+                roomColor = roomColor ?: "",
                 numOfRooms = Integer.parseInt(numOfRoomsStr ?: "0"))
             arguments.startActivity(context)
         }
